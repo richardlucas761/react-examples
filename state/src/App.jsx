351d461e7bccf24a8e434a3cea2472992a2144b4
Richard Lucas <richard@richardlucas.me.uk>
2026-06-20 14:59:59 +0100

    Add a new state variable to keep track of the number of times the background color has been changed.

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -5,28 +5,35 @@ const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple', 'cyan', 'aqua', 'mi
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [backgroundChanges, setBackgroundChanges] = useState(0);
 
   const onButtonClick = (color) => () => {
     setBackgroundColor(color);
+    setBackgroundChanges(backgroundChanges + 1);
   };
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor,
-      }}
-    >
-      {COLORS.map((color) => (
-        <button
-          type="button"
-          key={color}
-          onClick={onButtonClick(color)}
-          className={backgroundColor === color ? 'selected' : ''}
-        >
-          {color}
-        </button>
-      ))}
+    <div>
+      <div>
+        <p>Background changes: {backgroundChanges}</p>
+      </div>
+      <div
+        className="App"
+        style={{
+          backgroundColor,
+        }}
+      >
+        {COLORS.map((color) => (
+          <button
+            type="button"
+            key={color}
+            onClick={onButtonClick(color)}
+            className={backgroundColor === color ? 'selected' : ''}
+          >
+            {color}
+          </button>
+        ))}
+      </div>
     </div>
   );
 }
